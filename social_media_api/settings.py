39f934d4b6df0e77374220b34c873f15d66e1cb4
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-22 18:50:27 +0200

    changed Media_root for docker

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "vol/web/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
